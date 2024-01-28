268ca3ffd4a0efc02197e85b85373735e4bede24
Abinash Karmakar <codeAbinash@gmail.com>
2024-01-28 22:29:42 +0530

    Refactor App component structure

--- App.tsx
+++ App.tsx
@@ -14,26 +14,28 @@ const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <PaperProvider theme={LightTheme}>
+    <>
       <StatusBar
         barStyle="dark-content"
         backgroundColor={LightTheme.colors.surface}
       />
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="Update"
-            component={Update}
-            options={{ headerShown: false }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </PaperProvider>
+      <PaperProvider theme={LightTheme}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Update"
+              component={Update}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </>
   );
 };
 
